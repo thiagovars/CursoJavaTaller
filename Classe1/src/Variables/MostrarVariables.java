a07b812471864c39c25a1c6c72fa09836419d48b
Thiagovars <thiagovars@gmail.com>
2017-03-15 20:18:17 -0300

    Primeiro Projeto Variables
    Primeiro projeto criado para as classes de JAVA.

---
+++ Classe1/src/Variables/MostrarVariables.java
@@ -0,0 +1,22 @@
+package Variables;
+
+public class MostrarVariables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Trabajando con variables \"int\"");
+
+		int num1 = 50;
+		int num2 = 83;
+		System.out.println("numero 1"+num1);
+		System.out.println("numero 2"+num2);
+		System.out.println("Suma = "+(num1+num2));
+		System.out.println("Resta = "+(num1-num2));
+		System.out.println("Multiplicación = "+(num1*num2));
+		System.out.println("División = "+(num1/num2));
+	}
+
+}
