6b27086d98440071c98540c5558394eede02e41a
Thiagovars <thiagovars@gmail.com>
2017-03-15 21:55:20 -0300

    For, Switch e Scanner
    Utilizada a primeira biblioteca em java para usar Scanner de algum
    valor.

---
+++ Classe1/src/EstructurasDeControl/WhileForTabla.java
@@ -0,0 +1,21 @@
+package EstructurasDeControl;
+import java.util.Scanner;
+
+public class WhileForTabla {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner variable = new Scanner(System.in);
+		System.out.println("escribir el numero de tabla");
+		int tabla = variable.nextInt();
+		int x = 1;
+		while (x <= 10) {
+			System.out.println(x+" x " +tabla+ " = "+(x*tabla));
+			x++;
+		}
+	}
+
+}
