f3a46d88441a16e5b49f418e40ff3e156e83d089
Thiago Silveira <thiagovars@gmail.com>
2017-04-19 07:45:27 -0300

    Ejercicios de Herencia.

---
+++ Classe1/src/POO/empleado.java
@@ -0,0 +1,49 @@
+package POO;
+
+public class empleado {
+
+	private String nombre;
+	private String apellido;
+	private String cedula;
+	private int edad;
+	private boolean casado;
+	private double salario;
+	
+	public empleado() {}
+	
+	public String clasificacion() {
+		if	(this.edad >= 21) {
+			return "Principiante";
+		} else if (this.edad >=22 && this.edad <= 35) {
+			return "Intermedio";
+		} else {
+			return "Senior";
+		}
+	}
+	
+	public void showEmpleado() {
+		System.out.println("Nombre: " + this.nombre+"\n"+
+	                       "Apellido: " + this.apellido+"\n"+
+						   "Cedula: " + this.cedula+"\n"+
+	                       "Edad: " + this.edad+"\n"+
+						   "Casado: "+ this.casado()+"\n"+
+	                       "Salario: " + this.salario);
+	}
+	
+	public String casado() {
+		if (this.casado) {
+			return "Casado";
+		} else {
+			return "solteiro";
+		}
+	}
+	
+	public void aumentaSalario(int porcentaje) {
+		if (porcentaje > 0) {
+			this.salario = (this.salario * porcentaje) /100;
+			System.out.println("Aumento realizado con suceso.");
+		} else {
+			System.out.println("No es possíble aumentar con porcentaje CERO!");
+		}
+	}
+}
