a9cd505a44a66a0ed3dfba6eb84dc834db40b7a4
Thiago Silveira <thiagovars@gmail.com>
2017-07-06 01:32:11 -0300

    criação de telas de crud de usuário

--- ProjFinal/src/com/ght/pantallas/EdtAdmFuncionario.java
+++ ProjFinal/src/com/ght/pantallas/EdtAdmFuncionario.java
@@ -1,5 +1,6 @@
 package com.ght.pantallas;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +13,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class MainAdmFrame extends JFrame {
+public class EdtAdmFuncionario extends JFrame {
 
 	private JPanel contentPane;
 
@@ -23,7 +24,7 @@ public class MainAdmFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainAdmFrame frame = new MainAdmFrame();
+					EdtAdmFuncionario frame = new EdtAdmFuncionario();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -35,17 +36,14 @@ public class MainAdmFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MainAdmFrame() {
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setBounds(100, 100, 450, 451);
+	public EdtAdmFuncionario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
-		/**
-		 * TODO: verificar se não existe uma forma de encapsular a criação de menus!
-		 */
 		JMenuBar menu = new JMenuBar();
 		
 		JMenu archivo         = new JMenu("File");
--- ProjFinal/src/com/ght/pantallas/IncAdmFuncionario.java
+++ ProjFinal/src/com/ght/pantallas/IncAdmFuncionario.java
@@ -1,5 +1,6 @@
 package com.ght.pantallas;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +13,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class MainAdmFrame extends JFrame {
+public class IncAdmFuncionario extends JFrame {
 
 	private JPanel contentPane;
 
@@ -23,7 +24,7 @@ public class MainAdmFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainAdmFrame frame = new MainAdmFrame();
+					IncAdmFuncionario frame = new IncAdmFuncionario();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -35,17 +36,14 @@ public class MainAdmFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MainAdmFrame() {
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setBounds(100, 100, 450, 451);
+	public IncAdmFuncionario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
-		/**
-		 * TODO: verificar se não existe uma forma de encapsular a criação de menus!
-		 */
 		JMenuBar menu = new JMenuBar();
 		
 		JMenu archivo         = new JMenu("File");
